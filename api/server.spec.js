7d1efbcc91997a86bde04c02fc0149cbb0b003a5
Mashima <52222646+zimashima@users.noreply.github.com>
2020-01-31 12:59:47 -0500

    MVP

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,9 +1,16 @@
 const supertest = require("supertest")
 const server = require("./server")
 
-test("GET/ enpoint for server works", async()=> {
+test("GET/ endpoint for server works", async()=> {
     const res = await supertest(server).get("/")
     expect(res.status).toBe(200)
     expect(res.type).toBe("application/json")
     expect(res.body.message).toMatch(/welcome/i)
+})
+
+
+
+test("POST/ enpoint for jokes should not work", async()=> {
+    const res = await supertest(server).get("/api/jokes")
+    expect(res.status).toBe(401)
 })
