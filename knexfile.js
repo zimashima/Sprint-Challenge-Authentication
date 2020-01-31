321b85bd24373be1393c31f6c786ee5d4e39d5c2
Mashima <52222646+zimashima@users.noreply.github.com>
2020-01-31 11:08:34 -0500

    updated knexfile and added test, update dbConfig with environment, updated package.json

--- knexfile.js
+++ knexfile.js
@@ -9,4 +9,15 @@ module.exports = {
     },
     seeds: { directory: './database/seeds' },
   },
+
+  test: {
+    client: 'sqlite3',
+    connection: { filename: './database/test.db3' },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './database/migrations',
+      tableName: 'dbmigrations',
+    },
+    seeds: { directory: './database/seeds' },
+  }
 };
